237f50944cd0e333dc7bb1b888d692f959570b55
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2025-07-29 23:25:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (18.95%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,29 +1,36 @@
 class Solution {
+    int[][] memo ;
     public int calculateMinimumHP(int[][] dungeon) {
+        int row = 0;
+        int col = 0;
         int n = dungeon.length;
         int m = dungeon[0].length;
-        Integer[][] memo = new Integer[n][m];
-        return dp(0, 0, dungeon, memo);
-    }
-
-    public int dp(int r, int c, int[][] dungeon, Integer[][] memo) {
-        int n = dungeon.length;
-        int m = dungeon[0].length;
-
-        if (r >= n || c >= m) return Integer.MAX_VALUE;
-
-        if (memo[r][c] != null) return memo[r][c];
-
-        if (r == n - 1 && c == m - 1) {
-            return Math.max(1, 1 - dungeon[r][c]);
+        memo = new int[n][m];
+        for(int[] arr : memo){
+            Arrays.fill(arr,-1);
         }
+        return dp(row,col,n,m,dungeon);
+    }
+    public int dp(int r,int c,int n,int m,int[][] dungeon){
+        if(r==n-1 && c==m-1){
+            if(dungeon[r][c]>=0){
+                return 1;
+            }else{
+                return Math.abs(dungeon[r][c])+1;
+            }
+        }
+        if(r<0 || c<0 || r>=n || c>=m){
+            return Integer.MAX_VALUE;
+        }
+        if(memo[r][c]!=-1)return memo[r][c];
+        int right = 0;
+        int down = 0;
+        right = dp(r,c+1,n,m,dungeon);
+        down = dp(r+1,c,n,m,dungeon);
 
-        int right = dp(r, c + 1, dungeon, memo);
-        int down = dp(r + 1, c, dungeon, memo);
+        int val = Math.min(right,down) - dungeon[r][c];
+        return memo[r][c] =  Math.max(val,1);
+        
 
-        int need = Math.min(right, down) - dungeon[r][c];
-
-        memo[r][c] = Math.max(1, need);
-        return memo[r][c];
     }
 }
