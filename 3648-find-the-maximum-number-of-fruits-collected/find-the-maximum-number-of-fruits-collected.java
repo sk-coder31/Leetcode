9d2845aa0d408719c3043f0fc0133e1bbd73a0f8
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2025-08-08 01:06:56 +0530

    Time: 60 ms (28.57%) | Memory: 197.6 MB (60.00%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
@@ -1,60 +1,112 @@
 class Solution {
-    private static final int INF = Integer.MAX_VALUE;
-    private int n;
-    private int[][] fruits;
-    private int[][] memo;
-    private List<int[]> dirs;
-    private int dfs(int row, int col, int moves) {
-        if (row == n - 1 && col == n - 1) {
-            return (moves == 0) ? 0 : INF;
+    int[][] m2;
+    public int maxCollectedFruits(int[][] f) {
+        int n = f.length;
+        
+        int c1 = 0;
+        for(int i = 0; i < n; i++){
+            c1 += f[i][i];
         }
-        if (moves == 0 || row == col) {
-            return INF;
+        
+        m2 = new int[n][n];
+        for(int[] row : m2) Arrays.fill(row, -1);
+        int c2 = dp2(0, n-1, f, n-1, n);
+        for(int[] row : m2) Arrays.fill(row, -1);
+        int c3 = dp3(n-1, 0, f, n-1, n);
+        
+        return c1 + c2 + c3 - 2 * f[n-1][n-1];
+    }
+    
+    public int dp2(int r, int c, int[][] f, int mv, int n){
+        if(r == n-1 && c == n-1){
+            return f[r][c];
         }
-        if (memo[row][col] != -1) {
-            return memo[row][col];
+        
+        if(r >= n || c < 0 || c >= n || mv <= 0){
+            return Integer.MIN_VALUE;
         }
-        long best = -1;
-        for (int[] d : dirs) {
-            int nr = row + d[0], nc = col + d[1];
-            if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
-                int val = dfs(nr, nc, moves - 1);
-                if (val != INF) {
-                    best = Math.max(best, (long) val);
-                }
+        
+        if(r == c && !(r == n-1 && c == n-1)){
+            return Integer.MIN_VALUE;
+        }
+        if(m2[r][c] != -1){
+            return m2[r][c];
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+        if(r + 1 < n && c - 1 >= 0){
+            int res = dp2(r + 1, c - 1, f, mv - 1, n);
+            if(res != Integer.MIN_VALUE){
+                max = Math.max(max, res);
             }
         }
-        int result = (best < 0 ? INF : fruits[row][col] + (int) best);
-        memo[row][col] = result;
-        return result;
-    }
-
-    public int maxCollectedFruits(int[][] fruits) {
-        this.n = fruits.length;
-        this.fruits = fruits;
-
-        long first = 0;
-        for (int i = 0; i < n; i++) {
-            first += fruits[i][i];
+        
+        if(r + 1 < n){
+            int res = dp2(r + 1, c, f, mv - 1, n);
+            if(res != Integer.MIN_VALUE){
+                max = Math.max(max, res);
+            }
         }
-        dirs = Arrays.asList(
-            new int[]{1, -1},
-            new int[]{1, 0},
-            new int[]{1, 1}
-        );
-        memo = new int[n][n];
-        for (int[] row : memo) Arrays.fill(row, -1);
-        int second = dfs(0, n - 1, n - 1);
-
-       
-        dirs = Arrays.asList(
-            new int[]{-1, 1},
-            new int[]{0, 1},
-            new int[]{1, 1}
-        );
-        for (int[] row : memo) Arrays.fill(row, -1);
-        int third = dfs(n - 1, 0, n - 1);
-
-        return (int) (first + second + third);
+        
+        if(r + 1 < n && c + 1 < n){
+            int res = dp2(r + 1, c + 1, f, mv - 1, n);
+            if(res != Integer.MIN_VALUE){
+                max = Math.max(max, res);
+            }
+        }
+        
+        if(max != Integer.MIN_VALUE){
+            max += f[r][c];
+        }
+        
+        return m2[r][c] = max;
+    }
+    
+    public int dp3(int r, int c, int[][] f, int mv, int n){
+        if(r == n-1 && c == n-1){
+            return f[r][c];
+        }
+        
+        if(r < 0 || r >= n || c >= n || mv <= 0){
+            return Integer.MIN_VALUE;
+        }
+        
+        if(r == c && !(r == n-1 && c == n-1)){
+            return Integer.MIN_VALUE;
+        }
+        
+        if(m2[r][c] != -1){
+            return m2[r][c];
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+        if(r - 1 >= 0 && c + 1 < n){
+            int res = dp3(r - 1, c + 1, f, mv - 1, n);
+            if(res != Integer.MIN_VALUE){
+                max = Math.max(max, res);
+            }
+        }
+        
+        if(c + 1 < n){
+            int res = dp3(r, c + 1, f, mv - 1, n);
+            if(res != Integer.MIN_VALUE){
+                max = Math.max(max, res);
+            }
+        }
+        
+        if(r + 1 < n && c + 1 < n){
+            int res = dp3(r + 1, c + 1, f, mv - 1, n);
+            if(res != Integer.MIN_VALUE){
+                max = Math.max(max, res);
+            }
+        }
+        
+        if(max != Integer.MIN_VALUE){
+            max += f[r][c];
+        }
+        
+        return m2[r][c] = max;
     }
 }
