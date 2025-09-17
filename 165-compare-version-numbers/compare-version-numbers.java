f14120c27e1e1f0e6ac70c8aded762ac08e3f2ed
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2025-09-18 00:39:15 +0530

    Time: 1 ms (77.83%) | Memory: 41.5 MB (67.66%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] arr1 = version1.split("\\.");
+        String[] arr2 = version2.split("\\.");
+        int i = 0;
+        int j = 0;
+        // System.out.println(Arrays.toString(arr1)+" "+Arrays.toString(arr2));
+        while(i < arr1.length && j < arr2.length){
+            int n = Integer.parseInt(arr1[i]);
+            int m = Integer.parseInt(arr2[i]);
+            int cmp = Integer.compare(n,m);
+            // System.out.println(cmp);
+            if(cmp == 0){
+                i++;
+                j++;
+            }else{
+                return cmp;
+            }
+        }
+        while(i < arr1.length){
+            int x = Integer.parseInt(arr1[i]);
+            if(x<=0){
+                i++;
+                continue;
+            }else{
+                return 1;
+            }
+            
+        }
+        while(j < arr2.length){
+            int x = Integer.parseInt(arr2[j]);
+            if(x<=0){
+                j++;
+                continue;
+            }else{
+                return -1;
+            }
+            
+        }
+        return 0;
+    }
+}
