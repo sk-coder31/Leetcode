c7c9897a55fd5d70e26c027fd1b00338c4880a03
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2026-01-03 12:47:59 +0530

    Time: 113 ms (10.77%) | Memory: 45 MB (28.21%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
@@ -1,18 +1,18 @@
 class Solution {
-    List<String>list = Arrays.asList("rgr", "rgb", "rbr", "rbg", "grg", "grb", "gbr", "gbg", "brg", "brb", "bgr", "bgb");
-    int mod = 1_000_000_007;
-    int[][] memo;
+    List<String> list = Arrays.asList("rgr", "rgb", "rbr", "rbg", "grg", "grb", "gbr", "gbg", "brg", "brb", "bgr","bgb");
+        int[][] memo;
+        int mod = 1_000_000_007;
     public int numOfWays(int n) {
-        memo = new int[list.size()][n+1];
-        for(int [] r : memo){
-            Arrays.fill(r,-1);
+        this.memo = new int[list.size()][n+1];
+        int total = 0;
+        for(int[] arr : memo){
+            Arrays.fill(arr,-1);
         }
         int count = 0;
         for(int i = 0 ; i < list.size() ; i++){
             count = (count + dp(i,n-1))%mod;
         }
         return count;
-
     }
     public int dp(int prev_idx,int size){
         if(size==0){
@@ -23,7 +23,7 @@ class Solution {
         }
         int count = 0;
         String str1 = list.get(prev_idx);
-        for(int i = 0 ; i < list.size() ; i++){
+        for(int i = 0 ; i < list.size(); i++){
             String str2 = list.get(i);
             if(i!=prev_idx && ok(str1,str2)){
                 count = (count + dp(i,size-1))%mod;
@@ -32,12 +32,11 @@ class Solution {
         return memo[prev_idx][size] = count;
     }
     public boolean ok(String str1,String str2){
-        for(int i = 0 ; i < str1.length() ; i++){
+        for(int i = 0 ; i < str1.length();i++){
             if(str1.charAt(i)==str2.charAt(i)){
                 return false;
             }
         }
         return true;
     }
-
 }
