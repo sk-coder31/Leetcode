0ea54a6f6d8d8d4750ecb1245dfb7ace99437ba4
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2026-01-28 00:10:32 +0530

    Time: 153 ms (31.17%) | Memory: 276.4 MB (19.03%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -1,46 +1,43 @@
 class Solution {
     public int minCost(int n, int[][] edges) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        List<List<int[]>>adj = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
             adj.add(new ArrayList<>());
         }
+        for(int[] nn : edges){
+            int u = nn[0];
+            int v = nn[1];
+            int wt = nn[2];
 
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
-            adj.get(u).add(new int[]{v, wt});
-            adj.get(v).add(new int[]{u, 2 * wt}); 
+            adj.get(u).add(new int[]{v,wt});
+            adj.get(v).add(new int[]{u,2*wt});
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        int[] mincost = new int[n];
-        Arrays.fill(mincost, Integer.MAX_VALUE);
-        mincost[0] = 0;
-        pq.add(new int[]{0, 0});
+        PriorityQueue<int[]>pq = new PriorityQueue<>((int[] a,int[] b)->{
+            return a[1]-b[1];
+        });
+        int[] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[0] = 0;
+        pq.add(new int[]{0,0});
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int cost = curr[1];
 
-        boolean[] visited = new boolean[n];
-
-        while (!pq.isEmpty()) {
-            int[] node = pq.poll();
-            int curr_wt = node[0];
-            int curr_node = node[1];
-
-            if (visited[curr_node]) continue;
-            visited[curr_node] = true;
-
-            for (int[] adj_node : adj.get(curr_node)) {
-                int neigh = adj_node[0];
-                int weight = adj_node[1];
-
-                int newdist = curr_wt + weight;
-                if (newdist < mincost[neigh]) {
-                    mincost[neigh] = newdist;
-                    pq.add(new int[]{newdist, neigh});
-                }
+            if(cost>dist[node]){
+                continue;
             }
-        }
+            for (int[] nei : adj.get(node)) {
+                int next = nei[0];
+                int wt = nei[1];
 
-        return mincost[n - 1] == Integer.MAX_VALUE ? -1 : mincost[n - 1];
+                if (dist[next] > cost + wt) {
+                    dist[next] = cost + wt;
+                    pq.add(new int[]{next, dist[next]});
+                }
+            }   
+        }
+        return dist[n-1]==Integer.MAX_VALUE ? -1 : dist[n-1];
     }
 }
