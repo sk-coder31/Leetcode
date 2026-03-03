2885f2a88747619b2a9c1711e9f8b44e154f82da
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2026-03-03 20:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (87.38%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,25 +1,39 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        String final_ans = helper(n);
-        return final_ans.charAt(k-1);
+        return helper(n, k);
     }
-    public String helper(int n){
-        if(n==1){
-            return "0";
+
+    public char helper(int n, int k) {
+        if (n == 1) {
+            return '0';
         }
-        String prev_str = helper(n-1);
-        String prev = prev_str + "1"+ rev_in(prev_str);
-        return prev;
-    }
-    public String rev_in(String s){
-        StringBuilder sc = new StringBuilder();
-        for(int i = s.length()-1 ; i>=0 ; i--){
-            if(s.charAt(i)=='0'){
-                sc.append("1");
-            }else{
-                sc.append("0");
+
+        int len = (1 << n) - 1;
+        int mid = (len+1)/2;
+        if (k < mid) {
+            return helper(n - 1, k);
+        } else if (k == mid) {
+            return '1';
+        } else {
+            char ch = helper(n - 1, len - (k - 1));
+            if (ch == '0') {
+                return '1';
             }
+            else {
+                return '0';
+            }
+
         }
-        return sc.toString();
     }
 }
+
+// S1 = "0"
+// S2 = "0 1 1"
+// S3 = "011 1 001"
+// S4 = "0111001 1 0110001" n = 4 , k = 11
+
+// n = 4 , k = 15 - (k-1) = 5 1
+
+// n = 3 , k = 7 - (5-1) = 3 0
+
+// n = 3   , k = 3/3 == 1
