954b6763094bc77f900d0ac7bd20636ee322f46c
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2026-03-07 11:57:55 +0530

    Time: 30 ms (29.66%) | Memory: 46.7 MB (97.88%) - LeetSync

---
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int minFlips(String s) {
+        int size = s.length();
+        String str1 = "";//01010101
+        String str2 = "";//10101010
+        int i = 0;
+        int j = 0;
+        int res1 = 0;
+        int res2 = 0;
+        int min = Integer.MAX_VALUE;
+        char ch1 = '0';
+        char ch2 = '0';
+        while(j<2*size){
+            if(j % 2 == 0){
+                ch1 = '0';
+                ch2 = '1';
+            }else{
+                ch1 = '1';
+                ch2 = '0';
+            }
+
+
+            if(s.charAt(j%size)!=ch1){
+                res1++;
+            }
+            if(s.charAt(j%size)!=ch2){
+                res2++;
+            }
+            if(j - i + 1 > size){
+                char x , y;
+                if(i % 2 == 0){
+                    x = '0';
+                    y = '1';
+                }else{
+                    x = '1';
+                    y = '0';
+                }
+                if(s.charAt(i%size)!=x){
+                    res1--;
+                }
+                if(s.charAt(i%size)!=y){
+                    res2--;
+                }
+                i++;
+            }
+            if(j - i + 1 == size){
+                min = Math.min(min,Math.min(res1,res2));
+            }
+            j++;
+        }
+        return min;
+    }
+}
