1336e7bb32b7b678abd956f854d4b650c3ef2afc
A Sharavana Kumar <99302512+sk-coder31@users.noreply.github.com>
2026-03-16 20:54:58 +0530

    Time: 124 ms (26.47%) | Memory: 49.3 MB (19.12%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
@@ -3,38 +3,50 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        TreeSet<Integer> set = new TreeSet<>((a,b)->b-a);
+        TreeSet<Integer> set = new TreeSet<>((a, b) -> b - a);
 
-        for(int r = 0; r < m; r++){
-            for(int c = 0; c < n; c++){
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
                 set.add(grid[r][c]);
+                int k = 1;
+                while (k <= 100) {
+                    if (check(r, c, k, n, m)) {
+                        int sum = 0;
+                        for (int i = 0; i < k; i++)
+                            sum += grid[r - k + i][c + i];
+                        for (int i = 0; i < k; i++)
+                            sum += grid[r + i][c + k - i];
+                        for (int i = 0; i < k; i++)
+                            sum += grid[r + k - i][c - i];
+                        for (int i = 0; i < k; i++)
+                            sum += grid[r - i][c - k + i];
 
-                for(int k = 1; r+k < m && r-k >= 0 && c-k >= 0 && c+k < n; k++){
-
-                    int sum = 0;
-                    for(int i = 0; i < k; i++)
-                        sum += grid[r-k+i][c+i];
-                    for(int i = 0; i < k; i++)
-                        sum += grid[r+i][c+k-i];
-                    for(int i = 0; i < k; i++)
-                        sum += grid[r+k-i][c-i];
-                    for(int i = 0; i < k; i++)
-                        sum += grid[r-i][c-k+i];
-
-                    set.add(sum);
+                        set.add(sum);
+                    }
+                    k++; 
                 }
             }
         }
-
         int size = Math.min(3, set.size());
         int[] ans = new int[size];
 
         int i = 0;
-        for(int val : set){
-            if(i == size) break;
+        for (int val : set) {  
+            if (i == size)
+                break;
             ans[i++] = val;
         }
 
         return ans;
+    }
+
+    public boolean check(int r, int c, int k, int n, int m) {
+        if (r - k >= 0 && r - k < m &&  
+            r + k >= 0 && r + k < m &&
+            c - k >= 0 && c - k < n &&
+            c + k >= 0 && c + k < n) {
+            return true;
+        }
+        return false;
     }
 }
